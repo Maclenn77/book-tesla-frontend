e77d8f798248d96fa431191f00c42276758cd414
Maclenn77 <maclenn77@gmail.com>
2022-04-12 15:34:10 -0500

    Carousel

---
+++ src/components/Main.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Carousel from "./Carousel";
+
+const Main = () => {
+    return(
+        <main>
+            <div className="flex content-center">
+            <h1 className="flex-1 text-center">LATEST MODELS</h1>
+            </div>
+            <div className="flex content-center">
+            <h2 className="flex-1 text-center">LATEST MODELS</h2>
+            </div>
+            <div className="flex content-center">
+           <Carousel />
+            </div>
+        </main>
+    )
+}
+
+export default Main
