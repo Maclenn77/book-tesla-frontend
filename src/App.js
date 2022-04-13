24771aeddbc15511e9a444855f6a39c76b825219
Maclenn77 <maclenn77@gmail.com>
2022-04-13 16:55:28 -0500

    Solve linter errors

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
-import { Provider } from 'react-redux';
-import logo from './logo.svg';
-import './App.css';
-
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import { Provider } from 'react-redux';
+import './App.css';
 
 import Main from './components/Main';
 
 function App() {
   return (
-    // <Provider store={store}>
-        <Router>
-          <Routes>
-            <Route exact path="/" element={<Main />} />
-          </Routes>
-        </Router>
-      // </Provider>
+  // <Provider store={store}>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Main />} />
+      </Routes>
+    </Router>
+  // </Provider>
   );
 }
 
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,20 +1,18 @@
-import React from "react";
-import Carousel from "./Carousel";
+import React from 'react';
+import Carousel from './Carousel';
 
-const Main = () => {
-    return(
-        <main>
-            <div className="flex content-center">
-            <h1 className="flex-1 text-center">LATEST MODELS</h1>
-            </div>
-            <div className="flex content-center">
-            <h2 className="flex-1 text-center">LATEST MODELS</h2>
-            </div>
-            <div className="flex content-center">
-           <Carousel />
-            </div>
-        </main>
-    )
-}
+const Main = () => (
+  <main>
+    <div className="flex content-center">
+      <h1 className="flex-1 text-center">LATEST MODELS</h1>
+    </div>
+    <div className="flex content-center">
+      <h2 className="flex-1 text-center">LATEST MODELS</h2>
+    </div>
+    <div className="flex content-center">
+      <Carousel />
+    </div>
+  </main>
+);
 
-export default Main
+export default Main;
