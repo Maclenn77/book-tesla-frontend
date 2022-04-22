75b74f3f940391edf89234912ba392092060d359
Mouaz El Molkey <88675267+iammouaz@users.noreply.github.com>
2022-04-22 16:18:45 +0300

    Revert "Partner 1"

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,44 +1,17 @@
-import { Box } from '@chakra-ui/react';
-import logo from 'Assets/logo.png';
-import NavigationPanel from 'layout/NavigationPanel';
-import { useSelector } from 'react-redux';
-import AddCar from './AddCar';
-import Carousel from './Carousel';
-import Login from './Login';
-import MobileMenu from './MobileMenu';
-import RemoveCar from './RemoveCar';
-import RentalCars from './RentalCars';
-import RentCar from './RentCar';
+import Carousel from 'components/Carousel';
 
 export default function Main() {
-  const login = useSelector((state) => state.AuthReducer);
-  const view = useSelector((state) => state.ViewReducer);
-
   return (
     <>
-      <Box className="flex md:flex-col">
-        <Box className="w-3/12 md:w-full h-screen md:h-full border-r drop-shadow-lg">
-          <img src={logo} className="w-40 pl-8 md:hidden" alt="logo" />
-          <Box className="hidden absolute md:block">
-            <MobileMenu />
-          </Box>
-          <NavigationPanel />
-        </Box>
-        {login && (
-          <Box className="w-9/12 md:w-full h-screen md:h-min flex items-center h-screen">
-            {view === 'reserve' && <RentCar />}
-            {view === 'home' && <Carousel />}
-            {view === 'reservation' && <RentalCars />}
-            {view === 'add' && <AddCar />}
-            {view === 'remove' && <RemoveCar />}
-          </Box>
-        )}
-        {!login && (
-          <Box className="w-9/12 md:w-full h-screen">
-            <Login />
-          </Box>
-        )}
-      </Box>
+      <div className="flex content-center">
+        <h1 className="flex-1 text-center">LATEST MODELS</h1>
+      </div>
+      <div className="flex content-center">
+        <h2 className="flex-1 text-center">LATEST MODELS</h2>
+      </div>
+      <div className="flex content-center">
+        <Carousel />
+      </div>
     </>
   );
 }
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,18 +1,9 @@
 import thunk from 'redux-thunk';
 import { createStore, combineReducers, applyMiddleware } from 'redux';
-import AuthReducer from './Auth';
-import ViewReducer from './view';
-import RentalCars from './rent';
-import CarsReducer from './cars';
 
 const middlewares = [thunk];
 
-const reducer = combineReducers({
-  AuthReducer,
-  ViewReducer,
-  RentalCars,
-  CarsReducer,
-});
+const reducer = combineReducers({});
 
 const store = createStore(reducer, applyMiddleware(...middlewares));
 
